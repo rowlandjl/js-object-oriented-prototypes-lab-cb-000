168633cdd4e9fdf25a1f3477966d3e15b0de7430
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 00:25:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function BoardMember(name, homeState, training){
   BoardMember.prototype.approve = function(){
     return 'You can do that!'
   }
-  this.doCharity = function(){
+  BoardMember.prototype.doCharity = function(){
     return "I like to help people."
   }
 
